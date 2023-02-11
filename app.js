a02191049db7dca3bbaca3169010917ae28bee31
leeyujeong <lyj0047@naver.com>
2023-02-11 15:58:56 +0900

    타입 명시

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ function logName(name) {
     console.log(name);
 }
 logName("Jack");
+var studentID = 12345;
+var studentName = "Jenny Kim";
+var age = 21;
+var subject = "Javascript";
+var courseCompleted = false;
+function getStudentDetails(studentID) {
+    return null;
+}
--- app.ts
+++ app.ts
@@ -3,3 +3,19 @@ function logName(name: string) {
 }
 
 logName("Jack");
+
+let studentID: number = 12345;
+let studentName: string = "Jenny Kim";
+let age: number = 21;
+let subject: string = "Javascript";
+let courseCompleted: boolean = false;
+
+function getStudentDetails(studentID: number): {
+  studentID: number;
+  studentName: string;
+  age: number;
+  subject: string;
+  createDate: Date;
+} {
+  return null;
+}
